9418b83d6f8f9e52102fc4d37cba81ff06bd80b5
SUMER <sumersovitkar@outlook.com>
2024-08-09 01:07:37 +0900

    .Net Code

---
+++ website/SimpleWebApp/Controllers/HomeController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using SimpleWebApp.Models;
+using System.Data;
+using System.Diagnostics;
+
+namespace SimpleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            _configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View("Privacy");
+        }
+        public IActionResult Database()
+        {
+            var categories = GetData();
+            return View(categories);
+        }
+
+        private List<string> GetData()
+        {
+            var connString = _configuration["AZURE_SQL_CONNECTIONSTRING"];
+            var categories = new List<string>();
+
+            using (var sqlConn = new SqlConnection(connString))
+            {
+                using (var cmd = new SqlCommand()
+                {
+                    CommandText = "SELECT * from [SalesLT].[ProductCategory]",
+                    CommandType = CommandType.Text,
+                    Connection = sqlConn
+                })
+                {
+                    sqlConn.Open();
+
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            categories.Add((string)reader["Name"]);
+                        }
+                    }
+                }
+            }
+
+            return categories;
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ website/SimpleWebApp/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Logging.ClearProviders();
+builder.Logging.AddConsole();
+builder.Logging.AddAzureWebAppDiagnostics();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
