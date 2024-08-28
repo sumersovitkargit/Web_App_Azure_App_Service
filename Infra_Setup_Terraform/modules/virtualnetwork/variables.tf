cccdb3e561194dd32419312dc126ac2008554f95
SUMER SOVITKAR <130219318+sumersovitkargit@users.noreply.github.com>
2024-08-29 03:31:37 +0900

    Update variables.tf

--- Infra_Setup_Terraform/modules/virtualnetwork/variables.tf
+++ Infra_Setup_Terraform/modules/virtualnetwork/variables.tf
@@ -39,3 +39,8 @@ variable "subnets" {
   }))
   description = "List of objects that represent the configuration of each subnet."
 }
+
+variable "sql_administrator_login_password" {
+  description = "The administrator password of the SQL server"
+  type        = string
+}
