140a5c172731b692468126a56ce07333cd591efd
SUMER SOVITKAR <130219318+sumersovitkargit@users.noreply.github.com>
2024-10-13 22:10:36 +0900

    Update output.tf

--- Infra_Setup_Terraform/output.tf
+++ Infra_Setup_Terraform/output.tf
@@ -1,9 +1,9 @@
 output "resource_group_id" {
   description = "The ID of the Resource Group"
-  value       = module.resource_group.id
+  value       = module.resource_group.resource_group_id
 }
 
 output "resource_group_name" {
   description = "The Name of the Resource Group"
-  value       = module.resource_group.name
+  value       = module.resource_group.resource_group_name
 }
