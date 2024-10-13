ace2e10cc23cc9ad68b2ce5002546c726533c314
SUMER <sumersovitkar@outlook.com>
2024-10-13 22:58:35 +0900

    App Servic Plan

--- Infra_Setup_Terraform/variables.tf
+++ Infra_Setup_Terraform/variables.tf
@@ -15,3 +15,73 @@ variable "tags" {
   type        = map(string)
   default     = {}  # Optional: empty map for no tags
 }
+
+## App service plan
+# variables.tf
+
+variable "app_service_plan_name" {
+  description = "The name for the App Service Plan."
+  type        = string
+}
+
+variable "location" {
+  description = "The Azure Region where the App Service Plan will be created."
+  type        = string
+}
+
+variable "os_type" {
+  description = "The O/S type for the App Services to be hosted in this plan."
+  type        = string
+}
+
+variable "resource_group_name" {
+  description = "The name of the Resource Group where the App Service Plan should exist."
+  type        = string
+}
+
+variable "sku_name" {
+  description = "The SKU name for the App Service Plan."
+  type        = string
+}
+
+variable "sku_tier" {
+  description = "The SKU tier for the App Service Plan."
+  type        = string
+}
+
+# Optional Variables
+variable "app_service_environment_id" {
+  description = "The ID of the App Service Environment."
+  type        = string
+  default     = null
+}
+
+variable "maximum_elastic_worker_count" {
+  description = "Maximum number of workers in an Elastic SKU Plan."
+  type        = number
+  default     = null
+}
+
+variable "worker_count" {
+  description = "The number of Workers (instances) to be allocated."
+  type        = number
+  default     = null
+}
+
+variable "per_site_scaling_enabled" {
+  description = "Enable per-site scaling."
+  type        = bool
+  default     = false
+}
+
+variable "zone_balancing_enabled" {
+  description = "Enable zone balancing for the App Service Plan."
+  type        = bool
+  default     = null
+}
+
+variable "tags" {
+  description = "A mapping of tags for the App Service Plan."
+  type        = map(string)
+  default     = {}
+}
--- Infra_Setup_Terraform/vars/terraform.tfvars
+++ Infra_Setup_Terraform/vars/terraform.tfvars
@@ -4,3 +4,19 @@ tags = {
   Environment = "Dev"
   Owner       = "Solunist_Research_WebApp"
 }
+
+##  App Service Plan
+# terraform.tfvars
+
+app_service_plan_name         = "my-app-service-plan"
+location                      = "East US"
+os_type                       = "Windows"
+resource_group_name           = "my-resource-group"
+sku_name                      = "S1"
+sku_tier                      = "Standard"
+
+# Optional Variables
+worker_count                  = 2
+per_site_scaling_enabled      = true
+tags                          = { Environment = "Production" }
+# Leave other optional variables out to use their defaults
