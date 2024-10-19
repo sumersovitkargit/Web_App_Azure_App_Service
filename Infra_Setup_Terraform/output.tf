211bf16a4832a6d0322cdcd275f93eeb5b6c2878
SUMER <sumersovitkar@outlook.com>
2024-10-19 13:43:05 +0530

    version 10

--- Infra_Setup_Terraform/output.tf
+++ Infra_Setup_Terraform/output.tf
@@ -20,10 +20,10 @@ output "app_insights_connection_string" {
 
 output "identity_id" {
   description = "The ID of the User-Assigned Managed Identity."
-  value       = azurerm_user_assigned_identity.my_identity.id
+  value       = module.user_assigned_identity.my_identity.id
 }
 
 output "identity_principal_id" {
   description = "The principal ID of the User-Assigned Managed Identity."
-  value       = azurerm_user_assigned_identity.my_identity.principal_id
+  value       = module.user_assigned_identity.my_identity.principal_id
 }
